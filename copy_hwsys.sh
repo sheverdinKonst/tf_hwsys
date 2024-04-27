862c93e330a8d5f4a43efddc21bb4780ecc7d58a
sheverdin <sheverdin@fort-telecom.ru>
2024-04-27 17:58:21 +0500

    init commit

---
+++ copy_hwsys.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+cp  -r /home/sheverdin/Fort_Telecom/TFORTIS_APP/tf_hwsys/src/hwsys_ctrl /home/sheverdin/Fort_Telecom/t_fortis/t_fortis_pack/openwrt_tfortis_packages/tf_hwsys/src
+
+cp -r /home/sheverdin/Fort_Telecom/TFORTIS_APP/tf_hwsys/lib /home/sheverdin/Fort_Telecom/t_fortis/t_fortis_pack/openwrt_tfortis_packages/tf_hwsys/
